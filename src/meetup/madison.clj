ac75ac4bd27e54d671a50ba624af067f817b6b32
Eric Tobin <eatobin@gmail.com>
2021-02-08 16:05:38 -0600

    first

---
+++ src/meetup/madison.clj
@@ -0,0 +1,9 @@
+(ns meetup.madison)
+
+(defn verse-one
+  [color1 color2]
+  (str "Roses are " color1 ", violets are " color2 ","))
+
+(defn verse-two
+  [color3 who]
+  {:sugar-is color3 :and-so-are who})
---
+++ test/meetup/madison_test.clj
@@ -0,0 +1,11 @@
+(ns meetup.madison-test
+  (:require [clojure.test :refer [deftest is]]
+            [meetup.madison :as mm]))
+
+(deftest verse-one-test
+  (is (= "Roses are red, violets are blue,"
+         (mm/verse-one "red" "blue"))))
+
+(deftest verse-two-test
+  (is (= {:sugar-is "sweet", :and-so-are "you"}
+         (mm/verse-two "sweet" "you"))))
